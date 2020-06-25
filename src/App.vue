dd8ae6a3df2c1af46edcee833d43752c651e5e52
felioch06 <felipipe88806@gmail.com>
2020-06-25 15:12:18 -0500

    rick y morty

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mt-5">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="text-center font-weight-bolder">PERSONAJES DE RICK Y MORTY</h1>
+          <div class="jumbotron">
+
+            <div class="row row-cols-1 row-cols-md-3">
+
+              <div class="col mb-4" v-for="(item, index) of datos" :key="index">
+                <div class="card">
+                  <img :src="item.image" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ item.name }}</h5>
+                    <p class="card-text"><b>Genero:</b> {{ item.gender }}</p>
+                    <p class="card-text"><b>Especie:</b> {{ item.species }}</p>
+                    <p class="card-text"><b>Estado:</b> {{ item.status }}</p>
+                    <p class="card-text"><b>Localización:</b> {{ item.location.name }}</p>
+                    <p class="card-text"><b>Origen:</b> {{ item.origin.name }}</p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      datos: {}
+    }
+  },
+
+  methods: {
+    
+   api(){  
+         this.axios.get('https://rickandmortyapi.com/api/character')
+         .then(res => {
+
+           this.datos = res.data.results;
+
+           console.log(this.datos)
+         })
+         .catch(e => console.log(e))
+       }
+    
+  },
+
+     mounted(){
+     this.api()
+   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
